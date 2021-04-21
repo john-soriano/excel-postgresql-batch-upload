29f1100373f4f1b9e473be1ab1d856af45e9a66f
John Soriano <developer@johnsoriano.com>
2021-04-21 10:18:25 +0800

    Initial commit
    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # excel-postgresql-batch-upload
  Script for uploading batch files of excel in postgresql. Automatically calculate character value if column is string to optimized memory allocation.
+
+ # Installation
+
+1. git clone https://github.com/john-soriano/insert-batch.git
+2. cd insert-batch
+3. change parameters in the source code
+4. pip install psycopg2 pandas sqlalchemy openpyxl
+5. python script.py
---
+++ script.py
@@ -0,0 +1,55 @@
+import glob
+import sqlalchemy
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+db_param_dic = {
+    "host": "<host>",
+    "port": "5432",
+    "database": "<database>",
+    "user": "<dbuser>",
+    "password": "<password>"
+}
+
+# postgresql connection
+engine = create_engine(
+    'postgresql+psycopg2://%s:%s@%s:%s/%s' % (db_param_dic["user"], db_param_dic["password"], db_param_dic["host"], db_param_dic["port"], db_param_dic["database"]))
+
+# target table if doesn't exist will create automatically
+table = '<table>'
+
+# destination folder
+path = 'data'
+
+df = pd.DataFrame()
+filenames = glob.glob(path + "/*.xlsx")
+for f in filenames:
+    data = pd.read_excel(f, engine='openpyxl')
+    df = df.append(data)
+
+
+def dict_types():
+    # copy df but object is string only to compute text length for string and find max value and add 10 to evaluate varchar()
+    df_copy = df.copy().select_dtypes(include="object")
+    # columns of copied df
+    columns = df_copy.columns
+    # instanciate column type
+    column_dict = {}
+    # need to compute character length of column data
+    for col in columns:
+        df_copy[col] = df_copy[col].astype(str).apply(lambda x: len(x))
+
+    # create column dict type and add 10 to max value of column length
+    for col in columns:
+        max_value = int(df_copy[col].max())
+        column_dict.update({
+            col: sqlalchemy.types.VARCHAR(max_value + 10)
+        })
+
+    return column_dict
+
+
+if __name__ == "__main__":
+    outputdict = dict_types()
+    df.to_sql(table, engine, dtype=outputdict)
